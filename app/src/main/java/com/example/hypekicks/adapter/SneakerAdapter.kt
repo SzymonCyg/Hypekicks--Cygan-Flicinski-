9c7e424cd700361f784f2ed8d41009f650a6bb4b
Filip <filip.flicinski@cyfroweszkoly.pl>
2026-04-19 15:34:05 +0200

    włączenie bindingu

--- app/src/main/java/com/example/hypekicks/adapter/SneakerAdapter.kt
+++ app/src/main/java/com/example/hypekicks/adapter/SneakerAdapter.kt
@@ -1,43 +1,40 @@
 package com.example.hypekicks.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.hypekicks.R
+import com.example.hypekicks.databinding.ItemSneakerBinding
 import com.example.hypekicks.model.Sneaker
+
 
 class SneakerAdapter(
     private var sneakers: List<Sneaker>,
     private val onItemClick: (Sneaker) -> Unit
 ) : RecyclerView.Adapter<SneakerAdapter.SneakerViewHolder>() {
 
-    class SneakerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val ivSneaker: ImageView = itemView.findViewById(R.id.ivSneaker)
-        val tvBrand: TextView = itemView.findViewById(R.id.tvSneakerBrand)
-        val tvModel: TextView = itemView.findViewById(R.id.tvSneakerModel)
-    }
+    class SneakerViewHolder(val binding: ItemSneakerBinding)
+        : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SneakerViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_sneaker, parent, false)
-
-        return SneakerViewHolder(view)
+        val binding = ItemSneakerBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return SneakerViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: SneakerViewHolder, position: Int) {
         val sneaker = sneakers[position]
 
-        holder.tvBrand.text = sneaker.brand
-        holder.tvModel.text = sneaker.modelName
+        holder.binding.tvSneakerBrand.text = sneaker.brand
+        holder.binding.tvSneakerModel.text = sneaker.modelName
 
         Glide.with(holder.itemView.context)
             .load(sneaker.imageUrl)
-            .placeholder(R.drawable.ic_launcher_background)
-            .into(holder.ivSneaker)
+            .placeholder(android.R.drawable.ic_menu_gallery)
+            .into(holder.binding.ivSneaker)
 
         holder.itemView.setOnClickListener {
             onItemClick(sneaker)
