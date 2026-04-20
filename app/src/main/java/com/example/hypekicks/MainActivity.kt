703e6202d0fb1a0b87b3e13eb6ac422e12cbb278
Filip <filip.flicinski@cyfroweszkoly.pl>
2026-04-20 22:17:04 +0200

    wygląd adaptera

--- app/src/main/java/com/example/hypekicks/MainActivity.kt
+++ app/src/main/java/com/example/hypekicks/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.hypekicks.adapter.SneakerAdapter
 import com.example.hypekicks.databinding.ActivityMainBinding
+import com.example.hypekicks.model.Sneaker
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -25,5 +28,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        binding.recyclerSneakers.layoutManager = GridLayoutManager(this, 2)
+        val testData = listOf(
+            Sneaker("", "Nike", "Air Force 1", 500.0, 2020, ""),
+            Sneaker("", "Adidas", "Yeezy 350", 1200.0, 2021, "")
+        )
+        val adapter = SneakerAdapter(testData) { }
+        binding.recyclerSneakers.adapter = adapter
     }
 }
