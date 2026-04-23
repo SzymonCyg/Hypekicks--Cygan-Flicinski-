e31cfd0077f1542a8ecf388b2f03423bdd9de246
Filip <filip.flicinski@cyfroweszkoly.pl>
2026-04-23 22:23:50 +0200

    wyświetlanie danych i zamykanie aktywności ze szczegółami

--- app/src/main/java/com/example/hypekicks/SneakerDetailActivity.kt
+++ app/src/main/java/com/example/hypekicks/SneakerDetailActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.example.hypekicks.databinding.ActivitySneakerDetailBinding
-
+import com.example.hypekicks.model.Sneaker
 
 class SneakerDetailActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivitySneakerDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +20,30 @@ class SneakerDetailActivity : AppCompatActivity() {
         binding = ActivitySneakerDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.widokGlowny) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        binding.detailsCloseButton.setOnClickListener {
+            finish()
+        }
+
+
+        val sneaker = intent.getSerializableExtra("SNEAKER") as? Sneaker
+
+
+        sneaker?.let {
+            binding.tvBrandNModel.text = "${it.brand} ${it.modelName}"
+            binding.tvReleaseYear.text = it.releaseYear.toString()
+            binding.tvPrice.text = "${it.resellPrice} zł"
+
+            Glide.with(this)
+                .load(it.imageUrl)
+                .placeholder(android.R.drawable.ic_menu_gallery)
+                .into(binding.ivSneakerDetails)
+        }
     }
-
-
 }
